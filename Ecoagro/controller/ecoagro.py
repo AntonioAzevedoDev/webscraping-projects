74bace7d6a9b3353155c6b6a1878b4b6d880742d
Antonio Azevedo <antonio.azevedo@evtit.com>
2022-12-08 09:02:26 -0300

    Ecoagro changes

--- Ecoagro/controller/ecoagro.py
+++ Ecoagro/controller/ecoagro.py
@@ -25,122 +25,131 @@ def save_cra(browser, overwrite=False):
     aux_pages = 1
     scroll_value = 100
     aux_cra = 1
-    try:
-        button_next = browser.find_element(By.CSS_SELECTOR, '.next > a:nth-child(1)')
-        while WebDriverWait(browser, 30).until(
-                    EC.element_to_be_clickable(button_next)):
-            cra_list = browser.find_element(By.CSS_SELECTOR, '.emissao-list').find_elements(By.TAG_NAME, 'li')
-            for cra in cra_list:
-                if 'OPERAÇÃO' in cra.text:
-                    continue
-                else:
-
-                    cra_details_id = cra.find_elements(By.CLASS_NAME, 'column')
-                    aux_detail = 0
-                    for detail in cra_details_id:
-                        if aux_detail == 3:
-                            break
-                        else:
-                            if title != '':
-                                title += '-' + detail.text
-                            else:
-                                title = detail.text
-                    filepath = Path(
-                        pc.CRA_PATH) / f"s-{utils.hashed(title)}-{datetime.now().strftime('%Y%m%d')}.json"
-                    if not overwrite and filepath.exists():
-                        print(f'Já existe: {title}')
-                        sleep(2)
-                        title = ''
-                        continue
-
-                    else:
-                        try:
-                            aux_series = 1
-                            cra_text = ''
-                            doc_text = ''
-                            text_payload = ''
-                            while True:
-                                try:
-                                    ActionChains(browser).context_click(cra).key_down(Keys.CONTROL).click(cra).key_up(
-                                        Keys.CONTROL).perform()
-                                    break
-                                except Exception as cl:
-                                    if scroll_value == 1000:
-                                        scroll_value = 100
-                                        continue
-                                    last_scroll_value = scroll_value
-                                    scroll_value += 100
-                                    browser.execute_script(f"window.scrollTo({last_scroll_value}, {scroll_value})")
-                                    sleep(2)
-                                    continue
-                            sleep(3)
-                            browser.switch_to.window(browser.window_handles[1])
-                            sleep(2)
-                            try:
-                                series = Select(browser.find_element(By.CSS_SELECTOR, '#series'))
-                                quant_series = len(series.options)
-                            except Exception as s:
-                                series = browser.find_element(By.CSS_SELECTOR, '.serie')
-                                quant_series = 1
-                            if aux_series == quant_series:
-                                text_payload = get_data(browser, cra_text, doc_text, text_payload)
-                                sleep(1)
-                                print(f'salvar {title}')
-                                Path(pc.CRA_PATH).mkdir(parents=True, exist_ok=True)
-                                with open(filepath, "w", encoding="utf-8") as fp:
-                                    dump(text_payload, fp, ensure_ascii=False, indent=1)
-                                sleep(1)
-                                text_data = ''
-                                title = ''
-                                browser.close()
-                                browser.switch_to.window(browser.window_handles[0])
-                                sleep(2)
-                        except Exception as tp:
-                            print(tp)
-                        else:
-                            try:
-                                payload = {}
-                                while aux_series <= quant_series:
-                                    text_payload = get_data(browser, cra_text, doc_text, text_payload)
-                                    payload[f"Série {aux_series}"] = text_payload
-                                    aux_series += 1
-                                    text_payload = ''
-                                    pattern = re.compile(f'{aux_series}')
-                                    for option in series.options:
-                                        value = option.get_attribute('text')
-                                        if pattern.search(value):
-                                            option.click()
-                                            sleep(3)
-                                            series = Select(browser.find_element(By.CSS_SELECTOR, '#series'))
-                                            break
-
-                                print(f'salvar {title}')
-                                Path(pc.CRA_PATH).mkdir(parents=True, exist_ok=True)
-                                with open(filepath, "w", encoding="utf-8") as fp:
-                                    dump(payload, fp, ensure_ascii=False, indent=1)
-                                sleep(1)
-                                text_data = ''
-                                title = ''
-                                browser.close()
-                                browser.switch_to.window(browser.window_handles[0])
-                                sleep(2)
-                            except Exception as pay:
-                                print(pay)
-            while True:
-                try:
-                    button_next.click()
-                    break
-                except Exception as bu:
-                    last_scroll_value = scroll_value
-                    scroll_value += 150
-                    browser.execute_script(f"window.scrollTo({last_scroll_value}, {scroll_value})")
-                    sleep(2)
-
-            sleep(3)
-            scroll_value = 100
+    while True:
+        try:
             button_next = browser.find_element(By.CSS_SELECTOR, '.next > a:nth-child(1)')
-    except Exception as app:
-        print(app)
+            while WebDriverWait(browser, 30).until(
+                        EC.element_to_be_clickable(button_next)):
+                cra_list = browser.find_element(By.CSS_SELECTOR, '.emissao-list').find_elements(By.TAG_NAME, 'li')
+                for cra in cra_list:
+                    if 'OPERAÇÃO' in cra.text:
+                        continue
+                    else:
+
+                        cra_details_id = cra.find_elements(By.CLASS_NAME, 'column')
+                        aux_detail = 0
+                        for detail in cra_details_id:
+                            if aux_detail == 3:
+                                break
+                            else:
+                                if title != '':
+                                    title += '-' + detail.text
+                                else:
+                                    title = detail.text
+                        filepath = Path(
+                            pc.CRA_PATH) / f"s-{utils.hashed(title)}-{datetime.now().strftime('%Y%m%d')}.json"
+                        if not overwrite and filepath.exists():
+                            sleep(2)
+                            title = ''
+                            continue
+
+                        else:
+                            try:
+                                aux_series = 1
+                                cra_text = ''
+                                doc_text = ''
+                                text_payload = ''
+                                payload = {}
+                                while True:
+                                    try:
+                                        ActionChains(browser).context_click(cra).key_down(Keys.CONTROL).click(cra).key_up(
+                                            Keys.CONTROL).perform()
+                                        break
+                                    except Exception as cl:
+                                        if scroll_value == 1000:
+                                            scroll_value = 100
+                                            continue
+                                        last_scroll_value = scroll_value
+                                        scroll_value += 100
+                                        browser.execute_script(f"window.scrollTo({last_scroll_value}, {scroll_value})")
+                                        sleep(2)
+                                        continue
+                                sleep(3)
+                                browser.switch_to.window(browser.window_handles[1])
+                                sleep(2)
+                                try:
+                                    series = Select(browser.find_element(By.CSS_SELECTOR, '#series'))
+                                    quant_series = len(series.options)
+                                except Exception as s:
+                                    series = browser.find_element(By.CSS_SELECTOR, '.serie')
+                                    quant_series = 1
+                                if aux_series == quant_series:
+                                    text_payload = get_data(browser, cra_text, doc_text, text_payload)
+                                    payload[f"Série 1"] = text_payload
+                                    sleep(1)
+                                    print(f'salvar {title}')
+                                    Path(pc.CRA_PATH).mkdir(parents=True, exist_ok=True)
+                                    with open(filepath, "w", encoding="utf-8") as fp:
+                                        dump(payload, fp, ensure_ascii=False, indent=1)
+                                    sleep(1)
+                                    text_data = ''
+                                    title = ''
+                                    browser.close()
+                                    browser.switch_to.window(browser.window_handles[0])
+                                    sleep(2)
+                            except Exception as tp:
+                                print('')
+                            else:
+                                try:
+                                    payload = {}
+                                    while aux_series <= quant_series:
+                                        text_payload = get_data(browser, cra_text, doc_text, text_payload)
+                                        sleep(1)
+                                        payload[f"Série {aux_series}"] = text_payload
+                                        aux_series += 1
+                                        text_payload = ''
+                                        pattern = re.compile(f'{aux_series}')
+                                        browser.execute_script(f"window.scrollTo(0, 200)")
+                                        for option in series.options:
+                                            value = option.get_attribute('text')
+                                            if pattern.search(value):
+                                                option.click()
+                                                sleep(3)
+                                                series = Select(browser.find_element(By.CSS_SELECTOR, '#series'))
+                                                break
+                                    sleep(2)
+                                    print(f'salvar {title}')
+                                    Path(pc.CRA_PATH).mkdir(parents=True, exist_ok=True)
+                                    with open(filepath, "w", encoding="utf-8") as fp:
+                                        dump(payload, fp, ensure_ascii=False, indent=1)
+                                    sleep(1)
+                                    text_data = ''
+                                    title = ''
+                                    browser.close()
+                                    browser.switch_to.window(browser.window_handles[0])
+                                    sleep(2)
+                                except Exception as pay:
+                                    print('')
+                while True:
+                    try:
+                        button_next.click()
+                        break
+                    except Exception as bu:
+                        last_scroll_value = scroll_value
+                        scroll_value += 150
+                        browser.execute_script(f"window.scrollTo({last_scroll_value}, {scroll_value})")
+                        button_next = browser.find_element(By.CSS_SELECTOR, '.next > a:nth-child(1)')
+                        sleep(2)
+
+                sleep(3)
+                scroll_value = 100
+                button_next = browser.find_element(By.CSS_SELECTOR, '.next > a:nth-child(1)')
+            break
+        except Exception as app:
+            browser.close()
+            browser.switch_to.window(browser.window_handles[0])
+            sleep(2)
+            continue
 
 
 def get_data(browser, cra_text, doc_text, text_payload):
@@ -150,15 +159,18 @@ def get_data(browser, cra_text, doc_text, text_payload):
             if 'Cadastre-se e acompanhe as emissões' in cr.text:
                 continue
             elif 'Características' in cr.text:
+                emissor_name = browser.find_element(By.CSS_SELECTOR, '.nome').text
                 items = cr.find_elements(By.CLASS_NAME, 'item')
                 name = cr.find_element(By.TAG_NAME, 'p').text
                 for item in items:
                     item_details = item.find_elements(By.TAG_NAME, 'p')
                     if cra_text == '' and item_details[1].text != '':
-                        cra_text = f'"{item_details[0].text}":"{item_details[1].text}"'
+                        cra_text = f'"Emissor":"{emissor_name}",'
+                        cra_text += f'"{item_details[0].text}":"{item_details[1].text}"'
                     elif item_details[1].text == '':
                         if cra_text == '':
-                            cra_text = f'"{item_details[0].text}":"-"'
+                            cra_text = f'"Emissor":"{emissor_name}",'
+                            cra_text += f'"{item_details[0].text}":"-"'
                         else:
                             cra_text += f',"{item_details[0].text}":"-"'
                     else:
@@ -178,9 +190,27 @@ def get_data(browser, cra_text, doc_text, text_payload):
 
             elif 'Documentos da Oferta' in cr.text:
                 doc_list = cr.find_element(By.TAG_NAME, 'ul').find_elements(By.TAG_NAME, 'li')
+                sleep(1)
                 for doc in doc_list:
+                    scroll_value = 200
+                    url_list = []
+                    while True:
+                        try:
+                            doc.click()
+                            sleep(2)
+                            url_list = browser.find_element(By.CSS_SELECTOR, '.content-list').find_elements(By.TAG_NAME,
+                                                                                                            'li')
+                            break
+                        except Exception as cli:
+                            if scroll_value >= 1500:
+                                break
+                            last_scroll_value = scroll_value
+                            scroll_value += 150
+                            browser.execute_script(f"window.scrollTo({last_scroll_value}, {scroll_value})")
+                            sleep(2)
+                    sleep(1)
                     doc_name = doc.text
-                    url_list = browser.find_element(By.CSS_SELECTOR, '.content-list').find_elements(By.TAG_NAME, 'li')
+
                     if len(url_list) == 1:
                         if doc_text == '':
                             doc_text = f'"{doc_name}":"{url_list[0].find_element(By.TAG_NAME, "a").get_attribute("href")}"'
