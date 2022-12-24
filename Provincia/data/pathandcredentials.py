fbdf58c3628ce5e2895f52c086b1853d29f7b110
Antonio Azevedo <antonio.azevedo@evtit.com>
2022-12-24 09:09:27 -0300

    Commit Provincia Code

---
+++ Provincia/data/pathandcredentials.py
@@ -0,0 +1,2 @@
+CRA_PATH = r'C:\Users\antonio.azevedo\PycharmProjects\webscraping-projects\Provincia\provincia_data\br\cra'
+CRA_URL = 'https://www.provinciasecuritizadora.com.br/ProdutoDetalhe?Id=3'
